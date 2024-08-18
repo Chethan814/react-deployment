cbeceb6643080a5d2530182b99d822bf0dc78d3b
chethan <chethanchethu99339@gmail.com>
2024-08-18 07:02:08 +0530

    react and tailwind is setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='text-pretty text-center mt-10 text-blue-400 '>this project is to deployment of react and tailwind css</h2>
+    </>
+  )
+}
+
+export default App
